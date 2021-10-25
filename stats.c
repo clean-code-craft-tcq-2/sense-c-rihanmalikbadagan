bcd7cb3308bdb29856aa180004442e29d237899c
rihanmalikbadagan <91051471+rihanmalikbadagan@users.noreply.github.com>
2021-10-25 08:41:11 +0200

    Update stats.c
    compute statistics API implementation

--- stats.c
+++ stats.c
@@ -5,6 +5,23 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    s.min=s.max=numberset[0];
+    for(int i = 0; i < setlength ; i++)
+    {
+        s.average =+ numberset[i];
+        
+        if(s.min > numberset[i])
+        {
+            s.min = numberset[i];   
+        }
+        
+        if(s.max < numberset[i])
+        {
+            s.max = numberset[i];   
+        }
+    }
+    s.average = s.average/setlength;
+    
 }
 
 int emailAlertCallCount = 0;
